d1314a52c816feb338790dbdec3134e75877c667
kermitt PDX <bluefishbrowndirt@gmail.com>
2021-02-18 23:42:20 -0600

    added scrollbars

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import {
   Divider,
   Text,
   GridItem,
-  Grid
+  Grid,
+  Stack
 } from '@chakra-ui/react'
 // import { ColorModeSwitchButton } from './helpers/ColorModeSwitchButton.js'
 import { Header } from './helpers/Header.js'
@@ -20,30 +21,36 @@ export default function App() {
 
       <Flex
         direction={["column", "row"]}
-        border="1px solid purple"
-        height="50vh"
+        border="1px solid black"
+        height="80vh"
         overflow="hidden"
       >
         <Flex
           overflow="hidden"
           direction={["row", "column"]}
-          bg="blue.200"
           width={["100%", "25%"]}
-          height={["25%", "100%"]}
+          height={["10%", "100%"]}
         >
           <DocumentList />
 
         </Flex>
-        <Box bg="tomato" flex="1" overflow="auto">
-          Meow i see a bird i stare at it i meow at it i do a wiggle come here
-          birdy cat playing a fiddle in hey diddle diddle? peer out window,
-          chatter at birds, lure them to mouth purr purr purr until owner pets
-          why owner not pet me hiss scratch meow but rub face on owner wack the
-          mini furry mouse. Annoy the old grumpy cat, start a fight and then
-          retreat to wash when i lose cats secretly make all the worlds muffins
-        </Box>
-      </Flex>
+        <Stack w='100%'>
+          <Box bg="tomato" flex="1" overflow="auto">
 
+            This is a container
+
+            <DocumentList />
+
+          </Box>
+          <Box bg="tomato" flex="1" overflow="auto">
+            This is another container
+            <DocumentList />
+
+          </Box>
+        </Stack>
+
+      </Flex>
+      This is the bottom, still in App.js
 
     </ChakraProvider>
   )
